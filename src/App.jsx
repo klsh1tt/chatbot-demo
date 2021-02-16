8bf7276ac75afdf61faac8e5a2754c79b09416cb
klsh1tt <klsh1tt@gmail.com>
2021-02-16 18:48:02 +0900

    #5まで完了 繰り返し再利用できる関数コンポーネントを作ろう

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import defaultDataset from "./dataset";
-import './assets/styles/style.css'
+import defaultDataset from './dataset';
+import './assets/styles/style.css';
+import {AnswersList} from './components/index';
 
 export default class App extends React.Component {
   constructor(props){
@@ -11,14 +12,29 @@ export default class App extends React.Component {
       currentId: "init",
       dataset: defaultDataset,
       open: false
-    }
-
+    }  
   }
+
+  initAnswer = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+
+    this.setState({
+      answers:initAnswers
+    })
+  }
+
+  componentDidMount(){
+    this.initAnswer()
+  }
+
+
   render() {
     return (
       <section className="c-section">
         <div className="c-box">
-          {this.state.currentId}
+          <AnswersList answers={this.state.answers} />
+         
         </div>
 
       </section>
