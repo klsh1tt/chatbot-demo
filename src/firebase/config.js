10e88631af7cb5eb4dadbd009b2e2b0a5ca960bb
klsh1tt <klsh1tt@gmail.com>
2021-03-02 16:28:51 +0900

    完了 #10...Cloud FunctionsでAPIを作ってFirestoreを利用しよう

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD1mKURcv9MHg7siJqSGoxKkaYB8tND6Sw",
+    authDomain: "chatbot-demo-60be5.firebaseapp.com",
+    databaseURL: "https://chatbot-demo-60be5-default-rtdb.firebaseio.com",
+    projectId: "chatbot-demo-60be5",
+    storageBucket: "chatbot-demo-60be5.appspot.com",
+    messagingSenderId: "163634857138",
+    appId: "1:163634857138:web:7ce7c66761bad606a735ab",
+    measurementId: "G-1WK8X8H244"
+  };
+  
+  export default firebaseConfig
+  
